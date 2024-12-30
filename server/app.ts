29a6687d44affa4fd96de8663c098906bfef8684
poster-keisuke <ta.keisuke4@gmail.com>
2024-12-30 15:13:27 +0900

    for type safe server package

--- server/app.ts
+++ server/app.ts
@@ -7,9 +7,10 @@ const app = new Hono();
 
 app.use(logger());
 
-app.route('/api/expenses', expensesRoutes);
+const apiRoutes = app.basePath('api').route('/expenses', expensesRoutes);
 
 app.use('*', serveStatic({ root: './frontend/dist' }));
 app.get('*', serveStatic({ path: './frontend/dist/index.html' }));
 
 export default app;
+export type ApiRoutes = typeof apiRoutes;
